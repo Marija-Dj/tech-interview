56c171a122e51bec1ed67e40ea03ecd074cd21e6
Marija-Dj <67473583+Marija-Dj@users.noreply.github.com>
2023-10-19 14:06:28 +0200

    v1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        font1Pop: ["Poppins"],
+        font2Mon: ["Montserrat"],
+        font3PT: ["PT Serif"],
+      }
+    },
+  },
+  plugins: [],
+}
